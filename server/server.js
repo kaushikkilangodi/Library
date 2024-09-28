358daf52577662af3dc5a470e0cd4b17d345a8f2
kaushikkilangodi1 <kaushikkilangodi@gmail.com>
2024-09-29 00:55:04 +0530

    final

--- server/server.js
+++ server/server.js
@@ -17,5 +17,14 @@ app.use("/api/books",booksRoute);
 app.use("/api/issues",issuesRoute);
 app.use("/api/reports", reportsRoute);
 
+const path =require("path");
+__dirname=path.resolve();
+
+if (process.env.NODE_ENV ==="production"){
+    app.use(express.static(path.join(__dirname,"/client/build")));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,"client","build","index.html"));
+    })
+}
 
 app.listen(port,()=> console.log(`Node server is started at ${port}`));
